60b784d3378bbe444900002689b84b91d53f9d78
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-07 13:47:27 +0100

    add error 500

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.use(express.json())
 const PORT = process.env.PORT
 const HOST = process.env.HOST
 app.use(express.static('public'))
+
+app.use('/posts', (req, res, next) => {
+	throw new Error('something went wrong')
+})
+
 app.use('/posts', logger)
 const postsRouter = require('./routes/posts.js')
 
@@ -14,3 +19,8 @@ app.listen(PORT, (req, res) => {
 })
 app.use('/posts', postsRouter)
 app.use(notFound)
+
+app.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({ message: 'Internal Server Error' })
+})
