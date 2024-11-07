d94ff067c4f770000cd8c0317834e461cb9aac6f
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-07 14:38:34 +0100

    fix app.js and notmiddle

--- app.js
+++ app.js
@@ -2,27 +2,33 @@ const express = require('express')
 const app = express()
 const notFound = require('./middleware/notmiddle.js')
 const logger = require('./middleware/logger.js')
-app.use(express.json())
-const PORT = process.env.PORT
-const HOST = process.env.HOST
-app.use(express.static('public'))
-
-app.use('/posts', (req, res, next) => {
-	throw new Error('something went wrong')
-})
-
-app.use('/posts', logger)
 const postsRouter = require('./routes/posts.js')
 
-app.listen(PORT, (req, res) => {
-	console.log(`Server is running in ${HOST}:${PORT}`)
-})
-app.use('/posts', postsRouter)
-app.use(notFound)
+const PORT = process.env.PORT
+const HOST = process.env.HOST
 
+app.use(express.static('public'))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send('Hello World')
+})
+
+/*app.use('/posts', (req, res, next) => {
+	throw new Error('something went wrong')
+})*/
+
+app.use('/posts', logger)
+app.use('/posts', postsRouter)
+
+app.use(notFound)
 app.use((err, req, res, next) => {
 	console.log('Error: ', err.message)
 	console.error(err.stack)
 
 	res.status(500).json({ message: 'Something went wrong', error: err.message })
+})
+
+app.listen(PORT, (req, res) => {
+	console.log(`Server is running in ${HOST}:${PORT}`)
 })
--- middleware/notmiddle.js
+++ middleware/notmiddle.js
@@ -1,5 +1,5 @@
 const notFound = (req, res, next) => {
-	res.status(404).json({
+	res.status(404).send({
 		status: 404,
 		message: 'Not Found middleware'
 	})
