e5ef5a75da8966e13ad013ee4de965c2932800e6
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-07 13:44:07 +0100

    add logger and notmiddle

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
+const notFound = require('./middleware/notmiddle.js')
+const logger = require('./middleware/logger.js')
 app.use(express.json())
 const PORT = process.env.PORT
 const HOST = process.env.HOST
 app.use(express.static('public'))
-
+app.use('/posts', logger)
 const postsRouter = require('./routes/posts.js')
-
-app.use('/posts', postsRouter)
 
 app.listen(PORT, (req, res) => {
 	console.log(`Server is running in ${HOST}:${PORT}`)
 })
+app.use('/posts', postsRouter)
+app.use(notFound)
